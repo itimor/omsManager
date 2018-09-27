94fabfe2a736338e3ef950459ba35a3fd30f76d3
itimor <itimor@126.com>
2018-09-27 16:46:57 +0800

    fix oms.ini

--- omsBackend/omsBackend/settings/prod.py
+++ omsBackend/omsBackend/settings/prod.py
@@ -9,12 +9,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'oms',
         'USER': 'oms',
-        'PASSWORD': 'jjyy',
+        'PASSWORD': 'oms@123#',
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': '3306'
     }
 }
 
