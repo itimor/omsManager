2b6e036534eaeb1f75bcb26c79b5aa2311db438e
itimor <itimor@126.com>
2018-09-28 10:45:16 +0800

    add static

--- omsBackend/omsBackend/urls.py
+++ omsBackend/omsBackend/urls.py
@@ -9,11 +9,12 @@ from django.views.generic.base import TemplateView
 from omsBackend import settings
 from omsBackend.routerApi import router
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
-    url(r'^api/', include(router.urls)),
-    # 用户认证
-    url(r'^api/changepasswd/', PasswordChangeView.as_view(), name='changepasswd'),
-    url(r'^api/api-token-auth/', obtain_jwt_token, name='rest_framework_token'),
-    url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'', TemplateView.as_view(template_name="index.html")),
-]
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              [
+                  url(r'^api/', include(router.urls)),
+                  # 用户认证
+                  url(r'^api/changepasswd/', PasswordChangeView.as_view(), name='changepasswd'),
+                  url(r'^api/api-token-auth/', obtain_jwt_token, name='rest_framework_token'),
+                  url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                  url(r'', TemplateView.as_view(template_name="index.html")),
+              ]
