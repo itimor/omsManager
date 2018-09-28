b832637f8ed547da8d18de41fec47f18657e0ad6
itimor <itimor@126.com>
2018-09-28 11:41:07 +0800

    add static

--- omsFrontend/src/router/index.js
+++ omsFrontend/src/router/index.js
@@ -8,7 +8,7 @@ import { getToken, canTurnTo } from '@/libs/util'
 Vue.use(Router)
 const router = new Router({
   routes,
-  mode: 'history'
+  mode: 'hash'  // 项目中本来mode: 'history'
 })
 const LOGIN_PAGE_NAME = 'login'
 
