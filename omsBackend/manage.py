ec412f1a9316e7883cf0465fdb9c855a06a6c139
itimor <itimor@126.com>
2018-09-28 09:57:02 +0800

    fix oms.ini

--- omsBackend/manage.py
+++ omsBackend/manage.py
@@ -4,6 +4,11 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omsBackend.settings')
+
+    # 排错 “The maximum column size is 767 bytes”
+    from django.db.backends.mysql.schema import DatabaseSchemaEditor
+    DatabaseSchemaEditor.sql_create_table += " ROW_FORMAT=DYNAMIC"
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
