d166ae4d93baa3e0f6061243a3f950f514019f34
itimor <itimor@126.com>
2018-10-02 09:12:04 +0800

    update readme

--- omsBackend/tools/views.py
+++ omsBackend/tools/views.py
@@ -4,7 +4,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from tools.models import Upload, Sendmail, Sendmessage,FileUpload
+from tools.models import Upload, Sendmail, Sendmessage, FileUpload
 from tools.serializers import UploadSerializer, SendmailSerializer, SendmessageSerializer, FileUploadSerializer
 from users.models import User
 from tasks.tasks import send_to_skype, send_to_mail
