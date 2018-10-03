2f9a6e43ab6b954ca181273f120fb1e21c78a511
itimor <itimor@126.com>
2018-10-03 14:27:21 +0800

    add monitor

--- omsBackend/utils/monitor_domain.py
+++ omsBackend/utils/monitor_domain.py
@@ -3,6 +3,10 @@
 
 import requests
 import datetime
+import json
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 class OMSAPI(object):
@@ -60,14 +64,38 @@ class OMSAPI(object):
             return {'msg': 'send msg error'}
 
 
+# 判断过期时间
 def count_expire(expire_time, safe_day=30):
     s_day = datetime.datetime.now()
     d_day = datetime.datetime.strptime(expire_time, '%Y-%m-%d')
     d_diff = (d_day - s_day).days
-    if d_diff < safe_day:
+    if 1 < d_diff < safe_day:
         return True
     else:
         return False
+
+
+# 发送邮件函数
+def send_mail(MAIL_ACOUNT, to_list, sub, content):
+    me = MAIL_ACOUNT["mail_user"] + "<" + MAIL_ACOUNT["mail_user"] + "@" + MAIL_ACOUNT["mail_postfix"] + ">"
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = to_list
+    context = MIMEText(content, _subtype='html', _charset='utf-8')  # 解决乱码
+    msg.attach(context)
+    try:
+        smtpserver = smtplib.SMTP(MAIL_ACOUNT["mail_host"], 587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.login(MAIL_ACOUNT["mail_user"], MAIL_ACOUNT["mail_pass"])
+        print("successful login")
+        smtpserver.sendmail(me, to_list, msg.as_string())
+        smtpserver.close()
+        return {"code": 'success', "msg": "通知邮件发送成功"}
+    except Exception as e:
+        print(e)
+        return {"code": 'error', "msg": "通知邮件发送失败"}
 
 
 if __name__ == '__main__':
@@ -80,11 +108,19 @@ if __name__ == '__main__':
     for domain in domains:
         if count_expire(domain['expire_time']):
             print(f'{domain["name"]} expire')
-            expire_domains.append(domain)
+            expire_domains.append({"name": domain["name"], "expire_time": domain["expire_time"], "type": domain["type"],
+                                   "account": domain["dnsname"]})
         else:
             print(f'{domain["name"]} ok')
 
-        import time
-        time.sleep(1)
-
-    print(expire_domains)
+    # 发送邮件
+    MAIL_ACOUNT = {
+        "mail_host": "smtp.gmail.com",
+        "mail_user": "server@e-veb.com",
+        "mail_pass": "ouci4fae3Oow9vohZoh7o",
+        "mail_postfix": "e-veb.com",
+    }
+    to_list = 'kiven@e-veb.com'
+    sub = '过期域名'
+    if expire_domains:
+        send_mail(MAIL_ACOUNT, to_list, sub, json.dumps(expire_domains))
