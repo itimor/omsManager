71fc164ed5f74a020e17ca6ccd7dc6a44f3ebcfb
Timor <1050443@B2B.COM>
2018-11-08 14:26:24 +0800

    add

--- omsBackend/utils/cdnbest_api.py
+++ omsBackend/utils/cdnbest_api.py
@@ -1,84 +1,131 @@
 # -*- coding: utf-8 -*-
 # author: timor
 
-import requests
 import datetime
 import time
+import urllib
 from hashlib import md5
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import requests
 
 
 class CDNAPI(object):
-    def __init__(self, uid, t, vhost, sign, user_type='main'):
-        if user_type == 'proxy':
-            self.api_dir = 'https://www.cdnbest.com/api2/proxy/index.php/'
-        else:
-            self.api_dir = 'https://www.cdnbest.com/api2/user/index.php/'
-        self.api_url = 'https://www.cdnbest.com/api2/site/index.php/'
+    def __init__(self, uid, vhost):
+        self.api_url = 'http://itgocdn.com/api2/site/index.php/'
+        self.skey = "Wactat3CtTJxwwGT"
         self.uid = uid
-        self.t = t
         self.vhost = vhost
-        self.sign = sign
         self.__header = dict()
-        self.token_s_time = ''
         self.__token = self.get_token()
 
     def get_token(self):
         """
         登录获取token
         """
-        self.__header["Accept"] = "application/x-www-form-urlencoded"
+        t = str(int(time.time()))
+        m1 = md5()
+        m2 = md5()
+        m1.update((str(self.uid) + self.skey).encode(encoding='utf-8'))
+        m2.update((m1.hexdigest() + t).encode(encoding='utf-8'))
+
         data = {
             "uid": self.uid,
-            "t": self.t,
+            "t": t,
             "vhost": self.vhost,
-            "sign": self.sign
+            "sign": m2.hexdigest()
         }
-        url = self.api_dir + 'token'
-        print(data)
-        print(url)
+        url = self.api_url + 'token'
+        self.__header["Accept"] = "application/x-www-form-urlencoded"
         req = requests.post(url, data=data, headers=self.__header, verify=False)
         try:
             token = req.json()
             self.token_s_time = datetime.datetime.now()
-            return token
+            return token['token']
         except KeyError:
             raise KeyError
 
-    # def http_request(self, url, data):
-    #     """
-    #     接收请求，返回结果
-    #     """
-    #     self.__header["Accept"] = "application/json"
-    #     self.__header["Authorization"] = "Bearer " + self.__token
-    #     token_e_time = datetime.datetime.now()
-    #     print("token_e_time: %s" % token_e_time)
-    #     print("token_s_time: %s" % self.token_s_time)
-    #
-    #     if (token_e_time - self.token_s_time).seconds > 4000:
-    #         print("Bot token is Expired")
-    #         self.get_token()
-    #
-    #     # 传入data参数字典，data为None 则方法为get，有date为post方法
-    #     data["from"] = {"id": self.client_id}
-    #     req = requests.post(url, json=data, headers=self.__header, verify=False)
-    #     try:
-    #         return req.json()
-    #     except:
-    #         print(req.content)
+    def http_request(self, method, url, __data=None):
+        """
+        接收请求，返回结果
+        """
+        self.__header["Accept"] = "application/json"
+
+        data = {
+            'uid': self.uid,
+            'vhost': self.vhost,
+            'token': self.__token
+        }
+
+        if __data != None:
+            data.update(**__data)
+
+        params = urllib.parse.urlencode(data)
+        # 传入data参数字典，data为None 则方法为get，有date为post方法
+        if method == 'GET':
+            req = requests.get(url, params=params)
+        elif method == 'POST':
+            req = requests.post(url, data=data, headers=self.__header, verify=False)
+        elif method == 'PUT':
+            req = requests.put(url, data=data, headers=self.__header, verify=False)
+        elif method == 'DELETE':
+            req = requests.delete(url, data=data, headers=self.__header, verify=False)
+        else:
+            return {'msg': 'router mathod not match!'}
+
+        try:
+            return req.json()
+        except:
+            return req.json()
+
+    def getDomainList(self):
+        """
+        获得域名
+        :return:
+        """
+        method = 'POST'
+        url = self.api_url + 'domain/list'
+        return self.http_request(method, url)
+
+    def getFirewall(self):
+        """
+        获取防火墙配置
+        :return:
+        """
+        method = 'GET'
+        url = self.api_url + 'firewall'
+        return self.http_request(method, url)
+
+    def postFirewallWhiteips(self, id, ips):
+        """
+        添加/修改白名单ips
+        :param id:
+        :param ips:
+        :return:
+        """
+        method = 'POST'
+        url = self.api_url + 'firewall/whiteips'
+        data = {
+            "id": id,
+            "ips": ips
+        }
+        return self.http_request(method, url, data)
+
+    def deleteFirewallWhiteips(self, id):
+        """
+        删除白名单
+        :param id:
+        :return:
+        """
+        method = 'POST'
+        url = self.api_url + 'firewall/whiteips'
+        data = {
+            "id": id
+        }
+        return self.http_request(method, url, data)
 
 
 if __name__ == '__main__':
-    t = str(int(time.time()))
-    uid = 72910
-    skey = "Wactat3CtTJxwwGT"
-    m1 = md5()
-    m2 = md5()
-    m1.update((str(uid) + skey).encode(encoding='utf-8'))
-    m2.update((m1.hexdigest() + t).encode(encoding='utf-8'))
-    vhost = "itgocdn.com"
-    user_type = 'proxy'
-    cdn = CDNAPI(uid, t, vhost, m2.hexdigest())
-    print(cdn.get_token())
+    uid = 1001
+    vhost = "w8ikychrt.com"
+    cdn = CDNAPI(uid, vhost)
+    print(cdn.getDomainList())
